4ee05d9831188d0ecd2da9ae81ba5ded881dad38
cake-dev <developer.montana@gmail.com>
2022-03-30 18:14:39 -0600

    score calculations

--- sportsbook_1.py
+++ sportsbook_1.py
@@ -4,53 +4,134 @@ File: sportsbook_1.py
 Date: 03/30/22
 """
 import math
+import numpy
 import pandas as pd
 from sportsipy.ncaab.teams import Team
+from sportsipy.ncaab.boxscore import Boxscore
 
 
-team1 = 'duke'  # 'north-carolina'
-team2 = 'kansas'  # 'villanova'
+TEAM_1 = 'DUKE'  # 'NORTH-CAROLINA'
+TEAM_2 = 'KANSAS'  # 'VILLANOVA'
 
 
 # returns the values of predicted scores for each team, can be used for plusMinus and moneyLine (for predicted winner), as well as combined for overUnder bet
 # to predict score, use NCAA games
+# use tournament games from current NCAA tourney + conf tourney
 def predictScore(t1, t2):
-    pass
+    # jake
+    d1_avg_poss = 67.3
+    d1_avg_off = 103.8
+    team1 = Team(t1)
+    team2 = Team(t2)
+    schedule1 = team1.schedule
+    schedule2 = team2.schedule
+    sc1_df = schedule1.dataframe
+    sc2_df = schedule2.dataframe
+    sc1_adj = sc1_df[sc1_df.type != 'Reg']
+    sc2_adj = sc2_df[sc2_df.type != 'Reg']
+    t1_game_dates = []
+    t2_game_dates = []
+    t1_stats = {
+        'pace': [],
+        'off_eff': [],
+        'def_eff': []
+    }
+    t2_stats = {
+        'pace': [],
+        'off_eff': [],
+        'def_eff': []
+    }
+
+    t1_avg_stats = {
+        'avg_pace': 0,
+        'avg_off_eff': 0,
+        'avg_def_eff': 0
+    }
+    t2_avg_stats = {
+        'avg_pace': 0,
+        'avg_off_eff': 0,
+        'avg_def_eff': 0
+    }
+    for date in sc1_adj.boxscore_index:
+        t1_game_dates.append(date)
+    for date in sc2_adj.boxscore_index:
+        t2_game_dates.append(date)
+    for date in t1_game_dates:
+        game = Boxscore(date)
+        t1_stats['pace'].append(game.pace)
+        t1_stats['off_eff'].append(game.home_offensive_rating)
+        t1_stats['def_eff'].append(game.home_defensive_rating)
+    t1_stats['pace'].pop()
+    t1_stats['off_eff'].pop()
+    t1_stats['def_eff'].pop()
+    for date in t2_game_dates:
+        game = Boxscore(date)
+        t2_stats['pace'].append(game.pace)
+        t2_stats['off_eff'].append(game.home_offensive_rating)
+        t2_stats['def_eff'].append(game.home_defensive_rating)
+    t2_stats['pace'].pop()
+    t2_stats['off_eff'].pop()
+    t2_stats['def_eff'].pop()
+    t1_avg_stats['avg_pace'] = numpy.mean(t1_stats['pace'])
+    t1_avg_stats['avg_off_eff'] = numpy.mean(t1_stats['off_eff'])
+    t1_avg_stats['avg_def_eff'] = numpy.mean(t1_stats['def_eff'])
+    t2_avg_stats['avg_pace'] = numpy.mean(t2_stats['pace'])
+    t2_avg_stats['avg_off_eff'] = numpy.mean(t2_stats['off_eff'])
+    t2_avg_stats['avg_def_eff'] = numpy.mean(t2_stats['def_eff'])
+    adj_poss_1 = (t1_avg_stats['avg_pace'] * t2_avg_stats['avg_pace']) / d1_avg_poss
+    adj_poss_2 = (t1_avg_stats['avg_pace'] - d1_avg_poss) + (t2_avg_stats['avg_pace'] - d1_avg_poss) + d1_avg_poss
+    adj_off_1_t1 = (t1_avg_stats['avg_off_eff'] * t2_avg_stats['avg_def_eff']) / d1_avg_off
+    adj_off_1_t2 = (t2_avg_stats['avg_off_eff'] * t1_avg_stats['avg_def_eff']) / d1_avg_off
+    adj_off_2_t1 = (t1_avg_stats['avg_off_eff'] - d1_avg_off + t2_avg_stats['avg_def_eff'] - d1_avg_off) + d1_avg_off
+    adj_off_2_t2 = (t2_avg_stats['avg_off_eff'] - d1_avg_off + t1_avg_stats['avg_def_eff'] - d1_avg_off) + d1_avg_off
+    adj_poss = (adj_poss_1 + adj_poss_2) / 2
+    adj_off_t1 = (adj_off_1_t1 + adj_off_2_t1) / 2
+    adj_off_t2 = (adj_off_1_t2 + adj_off_2_t2) / 2
+    t1_score = (adj_off_t1 / 100) * adj_poss
+    t2_score = (adj_off_t2 / 100) * adj_poss
+    return [t1_score, t2_score]
 
 
 # line determined by combined predicted score
 def overUnder(bet, choice, line):
+    # jake
     pass
 
 
 # money line payouts determined by ratio of score difference times a constant
 def moneyLine(bet, choice, line):
+    # jake
     pass
 
 
 # line determined by difference in predicted scores (i.e. prediction for Duke vs Villanova is 75-72, Duke would have -3, Villa would get +3)
 def plusMinus(bet, choice, line):
+    # jake
     pass
 
 
 #  Will (top scoring player) score more than (average + 1)?
 def skilledProp1(bet, choice, line):
+    # nick
     pass
 
 
 # Will there be > (avg) turnovers?
 def skilledProp2(bet, choice, line):
+    # nick
     pass
 
 
 # Will the game go to overtime? (chance determined by history of overtime games in NCAA final)
 # take each NCAA finals game (by date, using Boxscore), check if it went to overtime, create line based on ratio
 def unskilledProp1(bet, choice, line):
+    # myles
     pass
 
 
 # How many fouls will there be in the first half? (over/under avg fouls per game/2)
 def unskilledProp2(bet, choice, line):
+    # myles
     pass
 
 
@@ -112,6 +193,9 @@ def main():
     print('Choose a bet type (number)')
     for i in range(len(bet_types)):
         print('{}\t{}'.format(i, bet_types[i]))
+    scores = predictScore(TEAM_1, TEAM_2)
+    for score in scores:
+        print(score)
 
 
 if __name__ == '__main__':
